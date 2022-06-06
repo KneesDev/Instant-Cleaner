9d53afac9c0145d20b0874af0b6296c4b9c8c663
Knees <knismesisstore@gmail.com>
2022-06-06 20:45:16 +0300

    Update Instant Cleaner.bat

--- Instant Cleaner.bat
+++ Instant Cleaner.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDBaSQGRGG6sOpEZ++Pv4Pq7i0MOQOMxbM/41rGPLuUBqkznephj33lV+A==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF25
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHWQ=
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbQs=
-::ZQ05rAF9IAHYFVzEqQIZJghATQqWfEi2A7QS7fqb
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDC2ML3+7CKFcy+T+7uSCsC0=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWHKK8UcjDB5CLA==
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATEVou/8PQUZwqHOXieA6NcuripvYo=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDBaSQGRGG6sOpEZ++Pv4Pq7i0MOQOMxbML41rGPLuUB1VO1O8ZjhzRfgM5s
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Instant Cleaner by KneesDev
 IF "%~1" == "normal" GOTO normal
